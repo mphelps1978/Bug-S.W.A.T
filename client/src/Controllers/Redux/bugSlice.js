2d225e80acca5f35406f9fdda417178b9ce5eece
mphelps <michaeljphelps1978@gmail.com>
2022-04-20 16:24:24 +0000

    finished reducers w/ dummy data and configured store

--- client/src/Controllers/Redux/bugSlice.js
+++ client/src/Controllers/Redux/bugSlice.js
@@ -1,23 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {retrieveBugs} from '../bugController'
+import { retrieveBugs } from '../bugController'
 
 const slice = createSlice({
-  
+
   name: "bug",
   initialState: [],
   reducers: {
     getBugs: state => retrieveBugs(),
-        
+
     createBug: (state, actions) => {
-      
+
     },
-    
+
     updateBug: (state, actions) => {
-      
+
     },
-    
+
     closeBug: (state, actions) => {
-      
+
     }
   }
 })
+
+export default slice.reducer
+
+export const { getBugs, createBug, updateBug, closeBug } = slice.actions
