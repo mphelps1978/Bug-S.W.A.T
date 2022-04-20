7a0d0ed0c602478b88d6fdf11e7c9fbebf10ec91
mphelps <michaeljphelps1978@gmail.com>
2022-04-20 16:24:39 +0000

    finished reducers w/ dummy data and configured store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Provider } from 'react-redux'
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
+
+// Reducers
+import authReducer from './Controllers/Redux/authSlice'
+import bugReducer from './Controllers/Redux/bugSlice'
+import userReducer from './Controllers/Redux/userSlice'
+
+
+// Configuring Store
+
+const reducer = combineReducers({
+  auth: authReducer,
+  bug: bugReducer,
+  user: userReducer
+})
+
+const store = configureStore({reducer})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <Provider store={store}><App/></Provider>
 )
